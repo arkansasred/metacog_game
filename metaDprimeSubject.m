e484f5a33f0310ff769e69699e41bd2c630cc58b
Josh Oberman <joshoberman@Joshs-MacBook-Pro.local>
2017-07-25 15:05:19 -0500

    organize folder structure

--- metaDprimeSubject.m
+++
@@ -1,8 +0,0 @@
-function metaD = metaDprimeSubject (subjectNumber)
-%subjectNumber = int2str(subjectNumber);
-file = strcat('Subject', subjectNumber, '/Generalization/responses.csv');
-fid = fopen(file);
-subjData = textscan(fid, '%d%d%d', 'delimiter', ',', 'HeaderLines', 1);
-fclose(fid);
-[nR_S1, nR_S2] = trials2counts(subjData{1},subjData{3}, subjData{2}, 4);
-metaD = type2_SDT_SSE(nR_S1, nR_S2)
